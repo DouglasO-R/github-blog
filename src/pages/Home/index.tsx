1e396a26b2066e3846b42179bf7b6b70f1322f44
douglasor <douglasor@outlook.com>
2023-02-16 10:06:20 -0300

    refactor(Separation of Corncerns):Separete logic of fetch data from UI

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,48 +1,14 @@
-import { useEffect, useState } from "react";
-
 import { List } from "../../components/List";
 import { Profile } from "../../components/Profile";
 import { Search } from "../../components/Search";
-
-import { Post } from "../../model/Posts";
-import { User } from "../../model/User";
+import { useGitHub } from "../../hooks/useGitHub";
 
 import { HomeContainer } from "./styles";
 
 
 
-
-
 export function Home() {
-    const [user, setUser] = useState<User>({} as User);
-    const [posts, setPosts] = useState<Post[]>([]);
-
-    const [filter, setFilter] = useState("");
-
-    const getUserProfile = async (username: string) => {
-        const response = await fetch(`https://api.github.com/users/${username}`);
-        const data = await response.json();
-        setUser(new User(data));
-    }
-
-    const getPosts = async (username: string) => {
-        const response = await fetch(`https://api.github.com/search/issues?q=${filter}%20repo:${username}/reactjs-github-blog-challenge`)
-        const data = await response.json();
-        const posts = await data.items.map((item: any) => new Post({ user_login: item.user.login, ...item }))
-        console.log(data);
-        setPosts(posts);
-    }
-
-    useEffect(() => {
-        getUserProfile("rocketseat-education");
-        getPosts("rocketseat-education")
-    }, [filter])
-
-    const HandleFilter = (filter: string) => {
-        setFilter(filter)
-    }
-
-
+    const { user, posts, HandleFilter } = useGitHub();
 
     return (
         <HomeContainer>
